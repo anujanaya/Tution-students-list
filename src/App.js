dea576eb6d2c8a6f34fa6251b4136b17a1cf4aa1
Nayak <anuja.nayak@capgemini.com>
2023-06-30 18:10:55 +0530

    print state value

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [name, setName] = useState("");
   const [emailid, setEmailid] = useState("");
   const [number, setNumber] = useState("");
-  
+
   const onChangeName = (e) => {
     setName(e.target.value)
     console.log(e.target.value);
@@ -19,6 +19,7 @@ function App() {
   }
   const submitbtn = () => {
     alert("Hello")
+    console.log({name}, {emailid}, {number})
   };
 
   return (
@@ -26,9 +27,9 @@ function App() {
       <h3> Name</h3>
       <input text placeholder="Enter Name" value={name} onChange={(e) => { onChangeName(e) }}></input>
       <h3>Email Id</h3>
-      <input text placeholder="Email Id" value={emailid} onchange={(e) => { onChangeEmail(e) }}></input>
+      <input text placeholder="Email Id" value={emailid} onChange={(e) => { onChangeEmail(e) }}></input>
       <h3>Contact Number</h3>
-      <input text placeholder="Contact Number" value={number} onchange={(e) => { onChangeNumber(e) }}></input>
+      <input text placeholder="Contact Number" value={number} onChange={(e) => { onChangeNumber(e) }}></input>
       <div>
         <button onClick={submitbtn}>Submit</button>
       </div>
