84e18fb04a69296f67f7aa45fc621cbc5804fdbc
Nayak <anuja.nayak@capgemini.com>
2023-06-30 19:31:18 +0530

    gender

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [name, setName] = useState("");
   const [emailid, setEmailid] = useState("");
   const [number, setNumber] = useState("");
+ const[gender, setGender] = useState("");
 
   const onChangeName = (e) => {
     setName(e.target.value)
@@ -16,10 +17,13 @@ function App() {
   };
   const onChangeNumber = (e) => {
     setNumber(e.target.value)
-  }
+  };
+  const onChangeGender = (e) =>{
+    setGender(e.target.value)
+  };
   const submitbtn = () => {
     
-  if(name!== "" && emailid!== "" && number!==""){
+  if(name!== "" && emailid!== "" && number!=="" && gender!=="" ){
     console.log({ name }, { emailid }, { number })
 }else {
   alert("Please fill in all the fields.");
@@ -32,6 +36,13 @@ function App() {
       <input text placeholder="Email Id" value={emailid} onChange={(e) => { onChangeEmail(e) }}></input>
       <h3>Contact Number</h3>
       <input number placeholder="Contact Number" value={number} onChange={(e) => { onChangeNumber(e) }}></input>
+      <h3>Gender</h3>
+      <select value={gender} onChange={onChangeGender}>
+        <option value="">Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="Other">Other</option>
+      </select>
       <div>
         <button onClick={submitbtn}>Submit</button>
       </div>
