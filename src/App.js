e2ae4a37eff347481a892fa449a785ddeb685e22
Nayak <anuja.nayak@capgemini.com>
2023-06-30 15:08:18 +0530

    button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 function App() {
 
   const [name, setName] = useState("Ashi");
+
+  const OnChangeBtnClick = () => {
+    setName("Anuja");
+  };
   return (
     <div>
       <div className="Heading">
-        Student List{setName}
+        Student List
       </div>
       <div>{name}</div>
+      <button onClick={OnChangeBtnClick}>Change Name</button>
     </div>
   );
 }
