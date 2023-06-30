5638d762479218ae3cc53b9dff04f4e86e9472ee
Nayak <anuja.nayak@capgemini.com>
2023-06-30 17:24:05 +0530

    create many functions

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import './App.css';
 
 function App() {
 
-  const [name, setName] = useState(" ");
-
+  const [name, setName] = useState("");
+  const [emailid, setEmailid] = useState("");
+  const [number, setNumber] = useState("");
   const onChangeName = (e) => {
-    console.log(e);
-    };
-    
+    setName(e.target.value)
+    console.log(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmailid(e.target.value)
+  };
+  const onChangeNumber = (e) => {
+    setNumber(e.target.value)
+  }
+  const submitbtn = () => {
+    alert("Hello")
+  };
+
   return (
     <div>
       <div>
         Name
       </div>
-      <input text placeholder="Enter Name" value={name} onChange={(e)=>{onChangeName(e)}}></input>
+      <input text placeholder="Enter Name" value={name} onChange={(e) => { onChangeName(e) }}></input>
+      <input text placeholder="Email Id" value={emailid} onchange={(e) => { onChangeEmail(e) }}></input>
+      <input text placeholder="Contact Number" value={number} onchange={(e) => { onChangeNumber(e) }}></input>
       <div>
-      <button>Submit</button>
+        <button onClick={submitbtn}>Submit</button>
       </div>
     </div>
   );
