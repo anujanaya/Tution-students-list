479f34bacdb0ca56c20d08efd87a0d1778b48d97
Nayak <anuja.nayak@capgemini.com>
2023-07-02 22:34:07 +0530

    table created

--- src/App.js
+++ src/App.js
@@ -21,6 +21,28 @@ function App() {
   return (
     <div>
       <h1>Axios Heading</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        <tr>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Megha</td>
+          <td>19</td>
+          <td>Female</td>
+        </tr>
+        <tr>
+          <td>Subham</td>
+          <td>25</td>
+          <td>Male</td>
+        </tr>
+      </table>
     </div>
   );
 }
