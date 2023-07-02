30586b6809231e0da408e75f4edae86491dc2642
Nayak <anuja.nayak@capgemini.com>
2023-07-02 21:07:18 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { useState, axios, useEffect } from "react";
+import React, {useEffect } from "react";
 import './App.css';
-
+import axios from 'axios'
 function App() {
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-  const [name, setName] = useState("");
-  const [emailid, setEmailid] = useState("");
-  const [number, setNumber] = useState("");
-  const [gender, setGender] = useState("");
-  const [feedback, setFeedback] = useState("");
-  const [date, setDate] = useState("");
-
-  const url = 'https://jsonplaceholder.typicode.com/todos';
-  
   useEffect(() => {
     axios.get(url)
       .then(function (response) {
@@ -21,35 +13,12 @@ function App() {
         // handle error
         console.log(error);
       })
-  });
+  },[]);
   return (
-    <div>
-      <h3>Date</h3>
-      <input type="date" value={date} onChange={onChangeDate}></input>
-      <h3> Name</h3>
-      <input text placeholder="Enter Name" value={name} onChange={(e) => { onChangeName(e) }}></input>
-      <h3>Email Id</h3>
-      <input text placeholder="Email Id" value={emailid} onChange={(e) => { onChangeEmail(e) }}></input>
-      <h3>Contact Number</h3>
-      <input number placeholder="Contact Number" value={number} onChange={(e) => { onChangeNumber(e) }}></input>
-      <h3>Gender</h3>
-      <select value={gender} onChange={onChangeGender}>
-        <option value="">Select Gender</option>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-        <option value="Other">Other</option>
-      </select>
-      <h3>Feedback</h3>
-
-      <textarea rows="4" cols="50" text placeholder="Feedback" value={feedback} onChange={(e) => { onChangeFeedback(e) }}></textarea>
-      <div>
-        <button onClick={submitbtn}>Submit</button>
-      </div>
-      <div>
-        <button onClick={clearbtn}>Clear</button>
-      </div>
-    </div>
-  );
+<div>
+  <h1>Axios Heading</h1>
+</div>
+      );
 }
 
 export default App;
