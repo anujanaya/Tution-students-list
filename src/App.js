54f209373a9d672ffdda3af7a374a4e50f2475a0
Nayak <anuja.nayak@capgemini.com>
2023-07-02 22:03:49 +0530

    aligned the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useEffect } from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import axios from 'axios'
 function App() {
-const url = 'https://jsonplaceholder.typicode.com/todos';
+  const url = 'https://jsonplaceholder.typicode.com/todos';
 
-const getAxiosData = () =>{
-  axios.get(url)
+  const getAxiosData = () => {
+    axios.get(url)
       .then(function (response) {
         console.log(response.data);
       })
@@ -13,16 +13,16 @@ const getAxiosData = () =>{
         // handle error
         console.log(error);
       })
-}
-useEffect(() => {
-   getAxiosData(); 
-  },[]);
+  }
+  useEffect(() => {
+    getAxiosData();
+  }, []);
 
   return (
-<div>
-  <h1>Axios Heading</h1>
-</div>
-      );
+    <div>
+      <h1>Axios Heading</h1>
+    </div>
+  );
 }
 
 export default App;
