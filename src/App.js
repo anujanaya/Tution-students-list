5864efde94596826591e3e6d28a35ba56f568e95
Nayak <anuja.nayak@capgemini.com>
2023-07-02 22:47:57 +0530

    remove function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { useEffect } from "react";
 import './App.css';
-import axios from 'axios'
-function App() {
-  const url = 'https://jsonplaceholder.typicode.com/todos';
 
-  const getAxiosData = () => {
-    axios.get(url)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-  }
-  useEffect(() => {
-    getAxiosData();
-  }, []);
+function App() {
 
   return (
     <div>
