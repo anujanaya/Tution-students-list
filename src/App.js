e46f913ef00164ebf835adfe71059677d523ab80
Nayak <anuja.nayak@capgemini.com>
2023-07-03 17:18:17 +0530

    separte table for girls

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 function App() {
   const data = [
     { name: "Anom", age: 19, gender: "Male", email: "anom@gmail.com" },
-    { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
     { name: "Subham", age: 25, gender: "Male", email: "subham@gmail.com" },
+    { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
+    { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
+]
+  const girls =[
+    { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
     { name: "Anuja", age: 39, gender: "Female", email: "anuja@gmail.com" },
     { name: "Meghna", age: 49, gender: "Female", email: "meghna@gmail.com" },
-    { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
     { name: "Megha", age: 64, gender: "Female", email: "megha@gmail.com" },
-    { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
-
   ]
   return (
     <div className="App" >
+      
       <table>
+      <h1>Boys Name List</h1>
         <tr>
           <th>Name</th>
           <th>Age</th>
@@ -23,6 +26,17 @@ function App() {
           <th>Email</th>
         </tr>
         {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+              <td>{val.email}</td>
+            </tr>
+          )
+        })}
+        <h2>Girls Name List</h2>
+         {girls.map((val, key) => {
           return (
             <tr key={key}>
               <td>{val.name}</td>
