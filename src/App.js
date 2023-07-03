a3654bccadca832fa629c24b3be2e06b7daafd28
Nayak <anuja.nayak@capgemini.com>
2023-07-03 15:55:03 +0530

    put data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './App.css';
 
-const data = [
-  { name: "Anom", age: 19, gender: "Male", email: "anom@gmail.com" },
-  { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
-  { name: "Subham", age: 25, gender: "Male", email: "subham@gmail.com" },
-  { name: "Anuja", age: 39, gender: "Female", email: "anuja@gmail.com" },
-  { name: "Meghna", age: 49, gender: "Female", email: "meghna@gmail.com" },
-  { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
-  { name: "Megha", age: 64, gender: "Female", email: "megha@gmail.com" },
-  { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
-
-]
 function App() {
+  const data = [
+    { name: "Anom", age: 19, gender: "Male", email: "anom@gmail.com" },
+    { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
+    { name: "Subham", age: 25, gender: "Male", email: "subham@gmail.com" },
+    { name: "Anuja", age: 39, gender: "Female", email: "anuja@gmail.com" },
+    { name: "Meghna", age: 49, gender: "Female", email: "meghna@gmail.com" },
+    { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
+    { name: "Megha", age: 64, gender: "Female", email: "megha@gmail.com" },
+    { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
+
+  ]
   return (
     <div className="App" >
       <table>
