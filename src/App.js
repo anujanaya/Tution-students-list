a327678ece6cec296ec6e9f985a44eb3d663a263
Nayak <anuja.nayak@capgemini.com>
2023-07-03 15:36:59 +0530

    email

--- src/App.js
+++ src/App.js
@@ -11,46 +11,55 @@ function App() {
           <th>Name</th>
           <th>Age</th>
           <th>Gender</th>
+          <th>Email</th>
         </tr>
         <tr>
           <td>Anom</td>
           <td>19</td>
           <td>Male</td>
+          <td>anom@gmail.com</td>
         </tr>
         <tr>
           <td>Megha</td>
           <td>19</td>
           <td>Female</td>
+          <td>megha@gmail.com</td>
         </tr>
         <tr>
           <td>Subham</td>
           <td>25</td>
           <td>Male</td>
+          <td>subham@gmail.com</td>
         </tr>
         <tr>
           <td>Anuja</td>
           <td>26</td>
           <td>female</td>
+          <td>anuja@gmail.com</td>
         </tr>
         <tr>
           <td>Steven</td>
           <td>30</td>
           <td>Male</td>
+          <td>steven@gmail.com</td>
         </tr>
         <tr>
           <td>Khushi</td>
           <td>20</td>
           <td>Female</td>
+          <td>khushi@gmail.com</td>
         </tr>
         <tr>
           <td>Amit</td>
           <td>32</td>
           <td>Male</td>
+          <td>amit@gmail.com</td>
         </tr>
         <tr>
           <td>Sai Pallavi</td>
           <td>30</td>
           <td>Female</td>
+          <td>sai@gmail.com</td>
         </tr>
       </table>
     </div>
