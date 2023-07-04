c4bbfe93fdb6649ef5b9247908076cc46aa2be81
Nayak <anuja.nayak@capgemini.com>
2023-07-04 17:30:57 +0530

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
-import {data} from './constant.js';
+import { data } from './constant.js';
 
 function App() {
+  const [gender, setGender] = useState("");
+  const [tableData, setTableData] = useState([]);
+
+  const onChangeGender = (e) => {
+    setGender(e.target.value);
+    if (e.target.value === "All") {
+      setTableData(data);
+      return;
+    }
+    const boysData = data.filter((item) => item.gender === e.target.value);
+    setTableData(boysData);
+  };
   const boysData = data.filter((item) => item.gender === "Male")
   const girlsData = data.filter((item) => item.gender === "Female")
   return (
     <div className="App" >
-
+      <h1>Students List</h1>
+      <h3>Select Students</h3>
+      <select value={gender} onChange={onChangeGender}>
+        <option value="">Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="All">All</option>
+      </select>
       <table>
         <h1>Boys Name List</h1>
         <tr>
@@ -16,7 +35,7 @@ function App() {
           <th>Gender</th>
           <th>Email</th>
         </tr>
-        {boysData.map((val, key) => {
+        {tableData.map((val, key) => {
           return (
             <tr key={key}>
               <td>{val.name}</td>
@@ -26,22 +45,12 @@ function App() {
             </tr>
           )
         })}
-        <h2>Girls Name List</h2>
-        {girlsData.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.age}</td>
-              <td>{val.gender}</td>
-              <td>{val.email}</td>
-            </tr>
-          )
-        })}
+
       </table>
 
 
     </div>
   );
-      }
+}
 
 export default App;
