2225983ba2a8e7d720a7fc8bcb2c263a90f38b5f
Nayak <anuja.nayak@capgemini.com>
2023-07-04 15:51:52 +0530

    importing exporting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
 import './App.css';
+import {data} from './constant.js';
 
 function App() {
-  const data = [
-    { name: "Anom", age: 19, gender: "Male", email: "anom@gmail.com" },
-    { name: "Subham", age: 25, gender: "Male", email: "subham@gmail.com" },
-    { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
-    { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
-    { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
-    { name: "Anuja", age: 39, gender: "Female", email: "anuja@gmail.com" },
-    { name: "Meghna", age: 49, gender: "Female", email: "meghna@gmail.com" },
-    { name: "Megha", age: 64, gender: "Female", email: "megha@gmail.com" },
-  ]
   const boysData = data.filter((item) => item.gender === "Male")
   const girlsData = data.filter((item) => item.gender === "Female")
   return (
@@ -51,6 +42,6 @@ function App() {
 
     </div>
   );
-}
+      }
 
 export default App;
---
+++ src/constant.js
@@ -0,0 +1,12 @@
+
+
+ export const data = [
+    { name: "Anom", age: 19, gender: "Male", email: "anom@gmail.com" },
+    { name: "Subham", age: 25, gender: "Male", email: "subham@gmail.com" },
+    { name: "Subhajit", age: 50, gender: "Male", email: "subhajit@gmail.com" },
+    { name: "Subham", age: 15, gender: "Male", email: "subham@gmail.com" },
+    { name: "Megha", age: 19, gender: "Female", email: "megha@gmail.com" },
+    { name: "Anuja", age: 39, gender: "Female", email: "anuja@gmail.com" },
+    { name: "Meghna", age: 49, gender: "Female", email: "meghna@gmail.com" },
+    { name: "Megha", age: 64, gender: "Female", email: "megha@gmail.com" },
+  ]
